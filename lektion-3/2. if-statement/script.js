5c0d2b27ca3fcfcb158956d38f501a9f890242be
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2020-12-11 14:11:23 +0100

    Lektion 3

--- lektion-3/2. if-statement/script.js
+++ lektion-3/2. if-statement/script.js
@@ -88,16 +88,95 @@
 
         
     (compare statment) ? true-actions : false-actions
- 
+
+    JS och andra språk                  Andra språk kan ha
+    --------------------------------------------------------
+    ==  lika med                        eq
+    <   mindre än                       lt
+    >   större än                       gt
+    !=  inte lika med                   not
+    <=  mindre eller lika med           le
+    >=  större eller lika med           ge
+
+    &&  och                             and
+    ||  eller                           or
+    !   inte lika med                   not
+
+    === lika med och samma datatyp
+    !== inte lika med och samma datatyp
+
+
 */
 
 
 
-var employees = [
-    { firstName: 'Hans', lastName: 'Mattin-Lassei', age: 36},
-    { firstName: 'Tommy', lastName: 'Mattin-Lassei', age: 38},
-    { firstName: 'Haithem', lastName: 'Smirani', age: 35},
-    { firstName: 'Joakim', lastName: 'Wahlström', age: 33},
-    { firstName: 'Anki', lastName: 'Mattin-Lassei', age: 42}
-]
+// if( 1 == 1) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
 
+
+
+// if (1 == 1) 
+//     console.log(true);
+// else 
+//     console.log(false);
+
+// (1 == 1) ? console.log(true) : console.log(false);
+
+
+
+// if(1 === '1') {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if(1 === '1' && 1 < 2) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if(1 === '1' & 1 < 2) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+// var employees = [
+//     { firstName: 'Hans', lastName: 'Mattin-Lassei', age: 36},
+//     { firstName: 'Tommy', lastName: 'Mattin-Lassei', age: 38},
+//     { firstName: 'Haithem', lastName: 'Smirani', age: 35},
+//     { firstName: 'Joakim', lastName: 'Wahlström', age: 33},
+//     { firstName: 'Anki', lastName: 'Mattin-Lassei', age: 42}
+// ]
+
+// let sorted = []
+// let letters = [];
+
+// employees.forEach(person => {
+//     let index = 0;
+//     const {firstName} = person
+
+//     if(sorted.length !== 0) {
+//         letters.push(firstName[0]);
+//         letters.sort();
+//     }
+
+//     index = letters.indexOf(firstName[0])
+//     sorted.splice(index, 0, person)
+// })
+// console.log(sorted)
+
+
+let value = 5;
+
+if(value === 1) { console.log('värdet är ett'); }
+else if(value === 2) {console.log('värdet är två');}
+else if(value === 3) {console.log('värdet är tre');}
+else if(value === 4) {console.log('värdet är fyra');}
+else if(value === 5) {console.log('värdet är fem');}
+else {console.log('finns inte i if-satsen');}
