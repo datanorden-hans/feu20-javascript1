a00b4f2ff87001ee606cb9a935ae7aaa36c6193d
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2020-12-18 14:15:47 +0100

    lektion-6

---
+++ lektion-6/Events/script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('#btnAlert');
+
+
+// btn.addEventListener('click', function() {
+//   alert('Warning!')
+//   console.log('warning warning')
+// })
+
+// btn.addEventListener('dblclick', () => {
+//   console.log('warning warning')
+// })
+
+// btn.addEventListener('mousedown', () => {
+//   console.log('mouse down')
+// })
+// btn.addEventListener('mouseup', () => {
+//   console.log('mouse up')
+// })
+
+// btn.addEventListener('mouseenter', () => {
+//   console.log('mouse enter')
+// })
+// btn.addEventListener('mouseleave', () => {
+//   console.log('mouse leave')
+// })
+
