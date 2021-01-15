21adc2ae42b905b64f862d4673eb7b5c28807cf6
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-15 14:46:54 +0100

    lektion-7

--- lektion-7/users/script.js
+++ lektion-7/users/script.js
@@ -1,7 +1,37 @@
 const form = document.querySelector('#form');
 const output = document.querySelector('#output');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
 
-const validateText = (input, error) => {
+let users = [];
+
+const renderUsers = () => {
+
+  output.innerHTML = '';
+  users.forEach(user => {
+
+    let html = `
+    <div class="user">
+      <div class="text">
+        <h3>${user.firstName} ${user.lastName}</h3>
+        <small>${user.email}</small>
+      </div>
+      <div class="buttons">
+        <button class="btn btn-primary">change</button>
+        <button class="btn btn-danger">delete</button>
+      </div>
+    </div>
+`
+
+    output.innerHTML += html
+  })
+}
+
+const validateText = id => {
+  const input = document.querySelector('#'+id);
+  const error = document.querySelector('#'+id+'-error');
+
   if(input.value === '') {
     error.innerText = 'Du måste ange ett namn';
     return false;
@@ -15,7 +45,10 @@ const validateText = (input, error) => {
   }
 }
 
-const validateEmail = (input, error) => {
+const validateEmail = id => {
+  const input = document.querySelector('#'+id);
+  const error = document.querySelector('#'+id+'-error');
+
   let regEx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
 
   if(regEx.test(input.value)) {
@@ -23,30 +56,52 @@ const validateEmail = (input, error) => {
     return true;
   } else {
     error.innerText = 'Du måste ange en giltig email address'
-    return false
+    return false;
   }
 }
 
-const validate = id => {
-  const input = document.querySelector('#'+id);
-  const error = document.querySelector('#'+id+'-error');
+const validate = () => {
 
-  if(input.type === "text") {
-    validateText(input, error)
-  }
-
-  if(input.type === "email") {
-    validateEmail(input, error)
-  }
+  document.querySelectorAll('input').forEach(input => {
+  
+    if(input.type === "text") {
+      validateText(input.id);
+      // console.log(validateText(input, error))
+    }
+  
+    if(input.type === "email") {
+      validateEmail(input.id);
+    }
+    
+  })
 }
+
+const createUser = (firstName, lastName, email) => {
+  let user = {
+    id: Date.now().toString(),
+    firstName,
+    lastName,
+    email
+  }
+
+  users.push(user);
+  console.log(users);
+}
+
+renderUsers();
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  document.querySelectorAll('input').forEach(input => {
-    validate(input.id);
-    // console.log(input.id);
-  })
+
+  validate();
+
+  if(validateText('firstName') && validateText('lastName') && validateEmail('email')) {
+    
+    createUser(firstName.value, lastName.value, email.value);
+    renderUsers();
+
+  }
 
 
 })
