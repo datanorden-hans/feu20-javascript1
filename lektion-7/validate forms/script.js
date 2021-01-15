f4ef97055fbebb73c79445c77a39cb01eb4b7fa6
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-15 12:06:18 +0100

    lektion-7

---
+++ lektion-7/validate forms/script.js
@@ -0,0 +1,79 @@
+const regForm = document.querySelector('#regForm');
+const email = document.querySelector('#email');
+const inputs = document.querySelectorAll('input');
+
+const validate = id => {
+  let input = document.querySelector(id);
+
+  if(input.value === '' || input.value.length < 2) {
+    input.classList.add('is-invalid');
+    input.classList.remove('is-valid');
+
+    input.focus();
+    return false;
+
+  } else {
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+    return true;
+  }
+}
+
+const validateEmail = (email) => {
+  if(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email.value)) {
+    email.classList.remove('is-invalid');
+    email.classList.add('is-valid');
+    return true
+
+  } else {
+    email.classList.add('is-invalid');
+    email.classList.remove('is-valid');
+    return false
+
+  }
+}
+
+
+
+inputs.forEach(input => {
+  input.addEventListener('keyup', function() {
+    // console.log(input.type);
+    if(input.type === "text") {
+      validate('#'+input.id);
+    } else if(input.type === "email") {
+      validateEmail(email)
+    }
+  })
+})
+
+// email.addEventListener('keyup', function() {
+//   validateEmail(email)
+// })
+
+
+regForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // console.log(e.currentTarget[1].id);
+
+  // validate('#firstName');
+  // validate('#lastName');
+  // validateEmail(email);
+
+  for(let i = 0; i < e.currentTarget.length; i++) {
+    if(e.currentTarget[i].type === "text") {
+      validate('#'+ e.currentTarget[i].id)
+    } else if(e.currentTarget[i].type === "email") {
+      validateEmail(email);
+    }
+  }
+
+  if(validate('#firstName') && validate('#lastName') && validateEmail(email)) {
+    console.log('Success')
+    // skicka data till databasen eller skapa en ny användare osv.
+    regForm.reset();
+  } else {
+    console.log('nope')
+  }
+
+})
